1586d5c8886d2ab8b019a5da55b89e1d8371dce3
Arpan Sarkar <asarkar@vectra.ai>
2024-07-16 09:05:47 -0700

    updated argument parsing

--- VectraMitreMapper.py
+++ VectraMitreMapper.py
@@ -1,7 +1,7 @@
 from Modules.Navigator import BuildVectraMitreLayerInfo, CreateMitreLayerFile
-from Modules.VectraAPIFunctions import RequestAccessToken
 import os
-import sys
+from Modules.Auth import VectraSaaSAuth
+from argparse import ArgumentParser
 
 def InitializationFileCheck():
     ###Check primary data files
@@ -13,16 +13,43 @@ def InitializationFileCheck():
     print("\nCheck: All primary files available")
 
 if __name__ == '__main__':
-    tenant_url = sys.argv[1]
-    client_id = sys.argv[2]
-    client_secret = sys.argv[3]
-    try:
-        layer_name = sys.argv[4]
-    except:
-        layer_name = "Vectra MITRE Detection Map"
+    parser = ArgumentParser(description='Build a MITRE map of detections in Vectra AI platform.')
+    parser.add_argument('-url', '--tenant_url', required=True)
+    parser.add_argument('-client', '--client_id', required=True)
+    parser.add_argument('-secret', '--client_secret', required=True)
+    parser.add_argument('-name', '--layer_name')
+    parser.add_argument('-by', '--detection_by')
+    parser.add_argument('-id', '--id')
+    parser.add_argument('-state', '--detection_state', choices=['active', 'fixed', 'all'])
+    
+    args = parser.parse_args()
 
-    request_url = tenant_url+"/api/v3.3"
+    tenant_url = args.tenant_url
+    client_id = args.client_id
+    client_secret = args.client_secret
+
+    detection_by = args.detection_by
+    id = args.id
+    state = args.detection_state
+
+    if args.layer_name == None:
+        layer_name = "Vectra AI Tenant Detections MITRE Map"
+    else:
+        layer_name = args.layer_name
+
+    # api request url
+    request_url = tenant_url+"api/v3.3"
+    
+    # perform initial file check
     InitializationFileCheck()
-    access_token = RequestAccessToken(tenant_url, client_id, client_secret)
-    techniques, total_triggered_techniques = BuildVectraMitreLayerInfo(access_token = access_token, request_url = request_url)
+    
+    # authenticate to tenant
+    auth = VectraSaaSAuth(tenant_url, client_id, client_secret)
+    auth.authenticate()
+    auth.authManager()
+
+    # get detection information from tenant
+    techniques, total_triggered_techniques = BuildVectraMitreLayerInfo(auth = auth, request_url = request_url, detection_by = detection_by, id = id, state = state)
+
+    # build mitre navigator file
     CreateMitreLayerFile(layer_name, techniques, total_triggered_techniques, tenant_url)
